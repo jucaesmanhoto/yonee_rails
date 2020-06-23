fd9740307d4b9d981ceaeb221ff6c4f4961bc31e
Isabela Porto <isamporto@hotmail.com>
2020-06-23 20:39:41 -0300

    Added sexual orientation reference in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_233343) do
+ActiveRecord::Schema.define(version: 2020_06_23_233828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,11 @@ ActiveRecord::Schema.define(version: 2020_06_23_233343) do
     t.string "sex"
     t.string "role"
     t.string "full_name"
+    t.bigint "sexual_orientation_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["sexual_orientation_id"], name: "index_users_on_sexual_orientation_id"
   end
 
+  add_foreign_key "users", "sexual_orientations"
 end
