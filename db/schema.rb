d7b76c7f33b382dadc4bccedaa3fbafd77480a53
Isabela Porto <isamporto@hotmail.com>
2020-06-23 22:48:45 -0300

    Runned the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_24_004527) do
+ActiveRecord::Schema.define(version: 2020_06_24_004755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +31,6 @@ ActiveRecord::Schema.define(version: 2020_06_24_004527) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
-
 
   create_table "products", force: :cascade do |t|
     t.string "name"
