ea80f1edb7ac75eb83189c2820991cf2af8db081
Anne Costa <anneizaura@gmail.com>
2020-06-23 21:49:01 -0300

    model de cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_24_002123) do
-
+ActiveRecord::Schema.define(version: 2020_06_24_004755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -25,7 +30,6 @@ ActiveRecord::Schema.define(version: 2020_06_24_002123) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
-
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -35,7 +39,6 @@ ActiveRecord::Schema.define(version: 2020_06_24_002123) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_reviews_on_post_id"
-
   end
 
   create_table "sexual_orientations", force: :cascade do |t|
@@ -72,10 +75,9 @@ ActiveRecord::Schema.define(version: 2020_06_24_002123) do
     t.index ["sexual_orientation_id"], name: "index_users_on_sexual_orientation_id"
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "posts", "users"
-
   add_foreign_key "reviews", "posts"
-
   add_foreign_key "stores", "users"
   add_foreign_key "users", "sexual_orientations"
 end
