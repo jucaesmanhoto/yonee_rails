962d8d0f55b93790062d7e659cbc037c7f761479
Isabela Porto <isamporto@hotmail.com>
2020-06-23 21:29:54 -0300

    Added validation in the post and store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ApplicationRecord
   belongs_to :user
   has_many :products, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
