08bfdb3158a6606839b9b45a9b1b0ff9009d745e
Anne Costa <anneizaura@gmail.com>
2020-06-23 21:25:52 -0300

    model review

--- db/migrate/20200624002123_create_reviews.rb
+++ db/migrate/20200624002123_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.interger :rate
+      t.integer :rate
       t.text :content
       t.references :post, null: false, foreign_key: true
 
