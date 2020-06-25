88185d169a1342d0bd411cafb8b038cf2a794d93
Isabela Porto <isamporto@hotmail.com>
2020-06-24 23:17:30 -0300

    Created routes of posts, reviews and user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-  resources :
+  
+  resources :posts do
+    resources :reviews, only: %i(create update destroy)
+  end
+  resources :users, only: %i(show)
 end
