a6ac9f6d7982353970bc59103b0a98541fd2ed24
Joao Esmanhoto <jucaesmanhoto@gmail.com>
2020-06-25 17:02:07 -0300

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+puts "Destroying all users"
+User.destroy_all
 puts "Destroying all orientations"
 SexualOrientation.destroy_all
 orientations = ["homosexual", "heterosexual"]
@@ -9,8 +12,7 @@ orientations.each do |orientation|
 end
 puts "Created #{orientations.length} orientations"
 
-puts "Destroying all users"
-User.destroy_all
+
 puts "Creating an test user"
 user = User.create(email: "teste@teste.com", password: "123456", username: "teste", sexual_orientation: SexualOrientation.first, full_name: "Teste Teste", role: "User", birth_date: DateTime.new(1990, 9, 8))
 puts "Setting user as admin"
