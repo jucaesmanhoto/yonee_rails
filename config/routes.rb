51a119ebaa58b1a57e32715aaec1daddb9d7f760
caoquerque <caoquerque@gmail.com>
2020-06-25 16:38:26 -0300

    Route changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root to: 'pages#home', 'users#index'
+  root to: 'pages#home'
   resources :users, only: [:index] do
     member do
       post :follow
