8fb9515380f85657a7a38a31413ef959e18eb2f0
Isabela Porto <isamporto@hotmail.com>
2020-06-25 15:44:24 -0300

    Create route to confirm adulthood, about, added column interest to user and connect some routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_031123) do
+ActiveRecord::Schema.define(version: 2020_06_25_184247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,16 +35,6 @@ ActiveRecord::Schema.define(version: 2020_06_25_031123) do
     t.string "category_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-
-  create_table "post_categories", force: :cascade do |t|
-    t.bigint "post_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_post_categories_on_category_id"
-    t.index ["post_id"], name: "index_post_categories_on_post_id"
   end
 
   create_table "follows", force: :cascade do |t|
@@ -141,6 +131,7 @@ ActiveRecord::Schema.define(version: 2020_06_25_031123) do
     t.string "full_name"
     t.bigint "sexual_orientation_id", null: false
     t.boolean "admin"
+    t.string "interest", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["sexual_orientation_id"], name: "index_users_on_sexual_orientation_id"
