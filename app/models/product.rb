a861196e6ba30a014cbd50cf202cdc867f503fb7
Isabela Porto <isamporto@hotmail.com>
2020-06-26 15:40:20 -0300

    Created post and user seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :store
   
-  has_many :product_categories
+  has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
-  has_many :cart_products
+  has_many :cart_products, dependent: :destroy
   has_many :cart, through: :cart_products
 
   validates :name, :cents, :amount, :store, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,20 +26,53 @@ puts "Destroying all users"
 User.destroy_all
 puts "Creating an test user"
 user = User.create(email: "teste@teste.com", password: "123456", username: "teste", sexual_orientation: SexualOrientation.first, full_name: "Teste Teste", role: "User", birth_date: DateTime.new(1990, 9, 8))
-puts "Setting user as admin"
+puts "Setting test user as admin"
 user.admin = true
 user.save!
+puts "Created user"
+puts "Creating an user"
+user1 = User.create(email: "afonso@hotmail.com", password: "123456", username: "afonsinho", sexual_orientation: SexualOrientation.first, full_name: "Afonso Pereira", role: "User", birth_date: DateTime.new(1996, 9, 12))
+puts "Created user"
+puts "Creating an user"
+user2 = User.create(email: "jesica@gmail.com", password: "123456", username: "jaacabou", sexual_orientation: SexualOrientation.last, full_name: "Jesica Nunes", role: "User", birth_date: DateTime.new(1980, 9, 10))
+puts "Created user"
+puts "Creating an user"
+user3 = User.create(email: "bob@yahoo.com", password: "123456", username: "bob", sexual_orientation: SexualOrientation.last, full_name: "Bob Marley", role: "User", birth_date: DateTime.new(1999, 12, 8))
 puts "Created user"
 
 puts "Creating user interest"
 tantra = UserInterest.create(user: user, category: Category.first)
+puts "Created user interest"
+puts "Creating user interest"
+massagem = UserInterest.create(user: user1, category: Category.last)
+puts "Created user interest"
+puts "Creating user interest"
+tantra1 = UserInterest.create(user: user1, category: Category.first)
+puts "Created user interest"
+puts "Creating user interest"
+tantra2 = UserInterest.create(user: user2, category: Category.first)
+puts "Created user interest"
+puts "Creating user interest"
+massagem1 = UserInterest.create(user: user3, category: Category.last)
 puts "Created user interest"
 
 puts "Creating a post"
 post1 = Post.create!(title: "titulo", content: "conteudo", status: "Submitted", user: user )
 puts "Created one post"
 puts "Creating a post"
-post2 = Post.create!(title: "titulo2", content: "conteudo2", status: "Approved", user: user )
+post2 = Post.create!(title: "titulo2", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", status: "Approved", user: user )
+puts "Created one post"
+puts "Creating a post"
+post3 = Post.create!(title: "titulo3", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", status: "Approved", user: user2 )
+puts "Created one post"
+puts "Creating a post"
+post4 = Post.create!(title: "titulo4", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", status: "Approved", user: user2 )
+puts "Created one post"
+puts "Creating a post"
+post5 = Post.create!(title: "titulo5", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", status: "Approved", user: user3 )
+puts "Created one post"
+puts "Creating a post"
+post6 = Post.create!(title: "titulo6", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", status: "Approved", user: user3 )
 puts "Created one post"
 
 puts "Creating post category"
@@ -48,6 +81,22 @@ puts "Created post category"
 
 puts "Creating post category"
 PostCategory.create!(post: post2, category: Category.last)
+puts "Created post category"
+
+puts "Creating post category"
+PostCategory.create!(post: post3, category: Category.first)
+puts "Created post category"
+
+puts "Creating post category"
+PostCategory.create!(post: post4, category: Category.last)
+puts "Created post category"
+
+puts "Creating post category"
+PostCategory.create!(post: post5, category: Category.first)
+puts "Created post category"
+
+puts "Creating post category"
+PostCategory.create!(post: post6, category: Category.last)
 puts "Created post category"
 
 puts "Successful seed"
